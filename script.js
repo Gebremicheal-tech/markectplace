572867890e8ad33248548bd6fe6ecc921abfa4ac
Gebremicheal-tech <geregtsadiqi@gmail.com>
2025-12-02 03:42:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let a =2;
-while(a<10){
-  console.log("Hello"+a);
-a++;
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
 }
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
 
 
